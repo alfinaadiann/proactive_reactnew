35b75dc8c2b295203908320e8731cea580592816
Rinnrg <rinogumilang2@gmail.com>
2024-11-19 21:55:48 +0700

    del config override

--- config-overrides.js
+++
@@ -1,6 +0,0 @@
-const { override, addBabelPlugin } = require('customize-cra');
-
-module.exports = override(
-    addBabelPlugin("@babel/plugin-proposal-nullish-coalescing-operator"),
-    addBabelPlugin("@babel/plugin-proposal-optional-chaining")
-);
